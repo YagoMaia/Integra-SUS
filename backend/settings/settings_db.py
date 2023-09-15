3c21530fb6195db0005cb04ecce5c26e3994ebef
Yago Maia <yagohenriquev123@gmail.com>
2023-09-15 10:49:51 -0300

    Arrumando estrutura do projeto

---
+++ backend/settings/settings_db.py
@@ -0,0 +1,5 @@
+DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/integraSUS"
+POSTGRES_HOST: str='localhost'
+POSTGRES_DB: str='integraSUS'
+POSTGRES_USER: str='postgres'
+POSTGRES_PASSWORD: str='postgres'
